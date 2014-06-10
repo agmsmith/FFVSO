6f12795c69e8356b2387aed4b6359f2fec93bff2
Alexander G. M. Smith <agmsmith@ncf.ca>
2014-06-10 01:49:50 +0000

    First version which runs, though it doesn't do much.

--- FFVSO.cpp
+++ FFVSO.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * $Header: /CommonBe/agmsmith/Programming/AGMSBayesianSpam/Server/RCS/AGMSBayesianSpamServer.cpp,v 1.74 2002/12/14 02:43:57 agmsmith Exp $
+ * $Header: /CommonBe/agmsmith/Programming/Fringe\040Festival\040Visitor\040Schedule\040Optimiser/RCS/FFVSO.cpp,v 1.1 2014/06/09 23:27:24 agmsmith Exp agmsmith $
  *
  * This is a web server CGI program for selecting events (shows) at the Ottawa
  * Fringe Theatre Festival to make up an individual's custom list.  Choices are
@@ -15,7 +15,9 @@
  * easier, and functions are listed in reverse dependency order so that forward
  * declarations (function prototypes with no code) aren't needed.
  *
- * $Log: AGMSBayesianSpamServer.cpp,v $
+ * $Log: FFVSO.cpp,v $
+ * Revision 1.1  2014/06/09 23:27:24  agmsmith
+ * Initial revision
  */
 
 /* Standard C Library. */
@@ -35,12 +37,41 @@
 
 
 /******************************************************************************
+ * Class which contains information about a single show.  Basically just the
+ * title and favourite status, since the show can have multiple venues and
+ * times.
+ */
+
+struct Show
+{
+  std::string m_ShowName;
+  bool m_IsFavourite;
+};
+
+typedef struct ShowStruct
+{
+  bool m_IsFavourite;
+    /* TRUE if the show is one of the user's favourite ones.  They get
+    highlighted differently and there is a count of favourite shows not yet
+    scheduled. */
+
+  int m_ScheduledCount;
+    /* Number of times this show has been selected in the schedule.  Normally
+    only zero or one.  If more than one then it's been overscheduled, unless
+    the user really wants to see it several times. */
+
+} ShowRecord, *ShowPointer;
+
+typedef std::map<std::string, ShowRecord> ShowMap;
+  /* A collection of all the shows, each one just listed once. */
+
+
+/******************************************************************************
  * Global variables, and not-so-variable things too.  Grouped by functionality.
  */
 
-static const char *g_FFVSOAppSignature =
-  "application/x-vnd.agmsmith.FringeFestivalVisitorScheduleOptimiser";
-
+ShowMap g_AllShowsMap;
+  /* A list of all the shows. */
 
 
 /******************************************************************************
@@ -49,5 +80,7 @@ static const char *g_FFVSOAppSignature =
 
 int main (int argc, char**)
 {
+  printf ("Content-Type: text/html\r\n\r\n");
+  printf ("The end.\n");
   return 0;
 }
