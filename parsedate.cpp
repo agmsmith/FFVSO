225e77b258cb25a2d13d5664e30d9ce2dafb9947
Alexander G. M. Smith <agmsmith@ncf.ca>
2014-06-17 20:03:17 +0000

    Added Haiku specific types.

--- parsedate.cpp
+++ parsedate.cpp
@@ -11,8 +11,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <OS.h>
-
+// Haiku specific #include <OS.h>
+#include <sys/types.h>
+typedef __uint8_t uint8;
+typedef __int8_t int8;
+typedef __int32_t int32;
+typedef __uint32_t uint32;
+typedef __int64_t bigtime_t;
+typedef int status_t;
+#define B_ERROR (-1)
+#define B_OK 0
 
 #define TRACE_PARSEDATE 0
 #if TRACE_PARSEDATE
